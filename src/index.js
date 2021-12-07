f1a7e2d61dbe56f93f266cb23b04da66f8f8eed9
Patrick Nekel <patric@kenzie.com.br>
2021-12-07 17:50:47 -0300

    :sparkles: Add: create account feature, along with verify method for avoid duplicity.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+  return response.status(201).send();
 });
+
 app.listen(3333);
